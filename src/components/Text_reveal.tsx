20afe718557edab1329aae0bf901783c4b8e00ba
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-09-18 06:42:20 +0000

    Add files via upload

---
+++ src/components/Text_reveal.tsx
@@ -0,0 +1,34 @@
+import TextReveal from "./magicui/text-reveal";
+import img1 from "../app/assets/img.png"
+import Image from 'next/image';
+
+
+export  default function TextRevealDemo() {
+  return (
+    <div className="mt-[6rem] lg:mt-[20rem] md:-mt-[10rem] sm:-mt-[10rem] xs:-mt-[10rem] grid lg:flex md:grid sm:grid text-xs:grid">
+
+    <div className='overflow-y-hidden  mb-[10rem] '>
+    <div className="container">
+    <div className="box">
+        <div className="content">
+          <Image 
+            src={img1}
+            alt="my_profile_pic"
+            width="900"
+            height="900"
+          />
+        </div>
+    </div>
+    </div>
+
+</div>
+    <div className="z-10 flex min-h-[16rem] -mt-[10rem] items-center font-light justify-center font-sans bg-black dark:bg-black">
+      <TextReveal text="Hi, I'm Kwadwo Nyarko based in Ghana, a software engineer extraordinaire! 
+                        Websites are my jam I conjure up digital magic one line of code at a time
+                        When I'm not geeking out over code, I'm crafting websites that makes you go WOW!
+                         " />
+    </div>
+            
+    </div>
+  );
+}
