6b6067af8da34c779909f3992fd6138f6caa0167
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-09-21 19:32:15 +0000

    Update Word_pull_up.tsx

--- src/components/Word_pull_up.tsx
+++ src/components/Word_pull_up.tsx
@@ -3,7 +3,7 @@ import WordPullUp from "./magicui/word-pull-up";
 export default function WordPullUpDemo() {
   return (
     <WordPullUp
-      className=" mt-20 -mb-[5rem] paytone_one text-wrap font-extrabold md:mx-4 lg:mx-5 font-sans md:tracking-tighter lg:tracking-tight p-5 overflow-x-hidden leading-[2.2rem] overflow-y-hidden text-white dark:text-white md:text-xl md:leading-[5rem] xl:text-[3rem] lg:text-[3rem] sm:text-xl text-[18px]"
+      className=" mt-20 -mb-[5rem] text-wrap font-extrabold md:mx-4 lg:mx-5 font-sans md:tracking-tighter lg:tracking-tight p-5 overflow-x-hidden leading-[2.2rem] overflow-y-hidden text-white dark:text-white md:text-xl md:leading-[5rem] xl:text-[3rem] lg:text-[3rem] sm:text-xl text-[18px]"
       words="Meet the brain behind the code"
     />
   );
