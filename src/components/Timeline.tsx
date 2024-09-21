ceaa8c80a781b98b7adcea499a80785aace77506
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-09-21 13:46:58 +0000

    Update Timeline.tsx

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -16,7 +16,7 @@ export default function TimelineDemo() {
              IPMC UNIVERSITY COLLEGE
           </h1>
 
-          <p className="text-white dark:text-neutral-200 text-lg md:text-sm xl:text-xl  font-normal mb-8">
+          <p className="text-cyan-50 text-[17px] dark:text-neutral-200 text-lg md:text-sm xl:text-xl  font-normal mb-8">
           I'm committed to enhancing software development skills in architecture, algorithms, 
           data science, and SDLC to design, develop, and deploy solutions that solve real-world problems.
           </p>
@@ -35,7 +35,7 @@ export default function TimelineDemo() {
              FREECODECAMP
           </h1>
         
-          <p className="text-white dark:text-neutral-200 text-lg md:text-sm font-normal xl:text-xl  mb-8">
+          <p className="text-cyan-50 text-[17px] dark:text-neutral-200 text-lg md:text-sm font-normal xl:text-xl  mb-8">
           Skilled in building dynamic web applications using HTML5, CSS3, JavaScript, and Responsive Web Design for dynamic and user-friendly web applications
           </p>
         
@@ -53,7 +53,7 @@ export default function TimelineDemo() {
              PRESBYTERIAN COLLEGE OF EDUCATION
           </h1>
           <div className="mb-8">
-            <h1 className="text-white xl:text-xl ">
+            <h1 className="text-cyan-50 text-[17px] xl:text-xl ">
                I possess expertise in communication, education, and teaching methods, utilizing active learning and technology integration
             </h1>
           </div>
