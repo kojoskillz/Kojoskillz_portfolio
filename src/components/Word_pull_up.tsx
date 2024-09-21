4e3d7236d0b8524b55b0f14b656923f1d48ad728
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-09-21 19:53:00 +0000

    Update Word_pull_up.tsx

--- src/components/Word_pull_up.tsx
+++ src/components/Word_pull_up.tsx
@@ -3,8 +3,8 @@ import WordPullUp from "./magicui/word-pull-up";
 export default function WordPullUpDemo() {
   return (
     <WordPullUp
-      className=" mt-20 -mb-[5rem] text-wrap font-extrabold md:mx-4 lg:mx-5 font-sans md:tracking-tighter lg:tracking-tight p-5 overflow-x-hidden leading-[2.2rem] overflow-y-hidden text-white dark:text-white md:text-xl md:leading-[5rem] xl:text-[3rem] lg:text-[3rem] sm:text-xl text-[18px]"
-      words="Meet the brain behind the code"
+      className=" mt-20 -mb-[5rem] text-wrap font-extrabold md:mx-4 lg:mx-5 font-sans -tracking-2 md:tracking-tighter lg:tracking-tight p-5 overflow-x-hidden leading-[2.2rem] overflow-y-hidden text-white dark:text-white md:text-[30px] md:leading-[5rem] xl:text-[4rem] lg:text-[3rem] sm:text-xl text-[25px]"
+      words="Meet the creator"
     />
   );
 }
