df95c5ed4cd07b61660a7dd05fcb68a813bed701
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-09-22 16:00:15 +0000

    Update Vortex.tsx

--- src/components/Vortex.tsx
+++ src/components/Vortex.tsx
@@ -3,20 +3,20 @@ import { Vortex } from "../components/ui/vortex";
 
 export default function VortexDemo() {
   return (
-    <div className="w-[calc(100%)] mx-auto rounded-md h-[30rem] overflow-hidden">
+    <div className="w-[calc(100%)] mx-auto rounded-md h-[30rem] overflow-hidden" id='contact'>
       <Vortex
         backgroundColor="black"
         className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
       >
-        <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
+        <h2 className="text-white text-3xl md:text-6xl mt-16 font-bold text-center">
           Want to work together?
         </h2>
-        <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
+        <p className="text-white text-md md:text-3xl max-w-xl mt-6 text-center">
           Don&apos;t be shy shoot me a message
         </p>
         <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
             <a href="">
-                 <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-xl text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
+                 <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
                      Shoot me
                  </button>
             </a>
@@ -33,7 +33,15 @@ export default function VortexDemo() {
              and deployed with
             <span className="font-bold "> Render</span>
          </h1>
+
+         <div className="mt-[20rem]">
+         <a href="#Home">
+          <h1 className="text-white font-bold hover:text-cyan-200 transition-all     text-center">Back to top &uarr;</h1>
+         </a>
       </div>
+      </div>
+
+
       </Vortex>
     
     </div>
