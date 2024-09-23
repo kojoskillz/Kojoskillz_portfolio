358e7bc168b23bac2fff1ad4c66d1a8bfee82b78
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-09-23 13:52:42 +0000

    Update Timeline.tsx

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -9,14 +9,14 @@ export default function TimelineDemo() {
       title: "2024",
       content: (
         <div>
-          <h1 className="text-cyan-300 uppercase dark:text-neutral-200 text-xl font-bold md:text-sm xl:text-4xl  mb-4">
+          <h1 className="text-cyan-300 uppercase dark:text-neutral-200 text-xl font-bold md:text-xl xl:text-4xl  mb-4">
              Bsc. Software Engineering
           </h1>
-          <h1 className="text-white dark:text-neutral-200 text-xl font-bold md:text-sm xl:text-4xl mb-4">
+          <h1 className="text-white dark:text-neutral-200 text-xl font-bold md:text-xl xl:text-4xl mb-4">
              IPMC UNIVERSITY COLLEGE
           </h1>
 
-          <p className="text-cyan-50 text-[15px] dark:text-neutral-200 text-lg md:text-sm xl:text-xl  font-normal mb-8">
+          <p className="text-cyan-50 text-[15px] dark:text-neutral-200 text-lg md:text-md xl:text-xl  font-normal mb-8">
             I'm committed to enhancing software development skills in architecture, algorithms, 
             data science, and SDLC to design, develop, and deploy solutions that solve real-world problems.
           </p>
@@ -28,14 +28,14 @@ export default function TimelineDemo() {
       title: "2022-2023",
       content: (
         <div>
-          <h1 className="text-cyan-300 uppercase dark:text-neutral-200 text-xl font-bold md:text-sm xl:text-4xl mb-4">
+          <h1 className="text-cyan-300 uppercase dark:text-neutral-200 text-xl font-bold md:text-xl xl:text-4xl mb-4">
              Certification
           </h1>
-          <h1 className="text-white dark:text-neutral-200 text-xl font-bold md:text-sm xl:text-4xl mb-4">
+          <h1 className="text-white dark:text-neutral-200 text-xl font-bold md:text-xl xl:text-4xl mb-4">
              FREECODECAMP
           </h1>
         
-          <p className="text-cyan-50 text-[15px] dark:text-neutral-200 text-lg md:text-sm font-normal xl:text-xl  mb-8">
+          <p className="text-cyan-50 text-[15px] dark:text-neutral-200 text-lg md:text-md font-normal xl:text-xl  mb-8">
           Skilled in building dynamic web applications using HTML5, CSS3, JavaScript, and Responsive Web Design for dynamic and user-friendly web applications
           </p>
         
@@ -46,14 +46,14 @@ export default function TimelineDemo() {
       title: "2017-2020",
       content: (
         <div>
-        <h1 className="text-cyan-300 uppercase dark:text-neutral-200 text-xl font-bold md:text-sm xl:text-4xl  mb-4">
+        <h1 className="text-cyan-300 uppercase dark:text-neutral-200 text-xl font-bold md:text-xl xl:text-4xl  mb-4">
              DBE. EDUCATION
           </h1>
-          <h1 className="text-white dark:text-neutral-200 text-xl font-bold md:text-sm xl:text-4xl  mb-4">
+          <h1 className="text-white dark:text-neutral-200 text-xl font-bold md:text-xl xl:text-4xl  mb-4">
              PRESBYTERIAN COLLEGE OF EDUCATION
           </h1>
           <div className="mb-8">
-            <h1 className="text-cyan-50 text-[16px] xl:text-xl ">
+            <h1 className="text-cyan-50 md:text-md text-[16px] xl:text-xl ">
                I possess expertise in communication, education, and teaching methods, utilizing active learning and technology integration
             </h1>
           </div>
