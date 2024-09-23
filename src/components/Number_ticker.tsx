77a6d985bbdb4b586ea350d1f18b4bf19716cf07
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-09-23 13:51:30 +0000

    Update Number_ticker.tsx

--- src/components/Number_ticker.tsx
+++ src/components/Number_ticker.tsx
@@ -3,7 +3,7 @@ import NumberTicker from "./magicui/number-ticker";
 const  NumberTickerDemo = () => {
   return (
     <div className="mb-10 mt-16">
-    <p className="whitespace-pre-wrap text-8xl m-auto  lg:flex md:grid sm:grid xs:grid grid place-content-center overflow-x-hidden overflow-y-hidden gap-10 font-medium tracking-tighter bg-black   text-white dark:text-white">
+    <p className="whitespace-pre-wrap text-8xl m-auto xl:flex lg:flex md:flex sm:grid xs:grid grid place-content-center overflow-x-hidden overflow-y-hidden gap-10 font-medium tracking-tighter bg-black   text-white dark:text-white">
      <div className="border-2 border-white md:w-[20rem] lg:w-[20rem]  grid place-content-center p-5 rounded-3xl  border-solid ">
          <NumberTicker value={165}  />
          <h1 className="text-white/80 font-sans text-[16px]  text-center tracking-wide font-light" >Projects Completed</h1>
