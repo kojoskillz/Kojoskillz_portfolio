23b39d232c68f472ce0a45924085ee5868ec9454
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-09-23 11:34:57 +0000

    Update Vortex.tsx

--- src/components/Vortex.tsx
+++ src/components/Vortex.tsx
@@ -16,7 +16,7 @@ export default function VortexDemo() {
         </p>
         <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
             <a href="">
-                 <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
+                 <button className="px-4 py-2 buttonX bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
                      Shoot me
                  </button>
             </a>
