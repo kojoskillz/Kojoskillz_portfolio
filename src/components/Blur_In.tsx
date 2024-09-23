18ad9aa8336cd42722afeca819dad38422aa81ce
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-09-23 13:50:53 +0000

    Update Blur_In.tsx

--- src/components/Blur_In.tsx
+++ src/components/Blur_In.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 const BlurInDemo = () => {
   return (
 
-    <div className="grid mt-20 xl:flex lg:flex md:grid sm:grid xs:grid ">
+    <div className="grid mt-20  xl:flex lg:flex md:flex sm:grid xs:grid ">
 
     <div className="container">
     <div className="box">
@@ -27,10 +27,10 @@ const BlurInDemo = () => {
       word=" Hi, I'm Kwadwo Nyarko based in Ghana, a software engineer extraordinaire! Specialized in Frontend-Engineering.
              Websites are my jam I conjure up digital magic one line of code at a time.
               When I'm not geeking out over code, I'm crafting websites that makes you go WOW!"
-      className="font-semi-bold text-white mx-5 mt-5 dark:text-white"
+      className="font-semi-bold text-white lg:font-light mx-5 md:mx-0 lg:mx-[10rem] lg:ml-[-10rem] mt-5 dark:text-white"
     />
      {/* button for cv */}
-     <button className="buttonDownload buttonX z-10 mt-[42rem] ml-[1rem] xl:mt-[20rem] xl:ml-[25rem] lg:mt-[20rem] md:mt-[80rem] sm:mt-[30rem] xs:mt-[20rem]  xl:grid lg:grid md:grid sm:grid xs:grid">Download CV</button>
+     <button className="buttonDownload z-10 mt-[42rem] ml-[1rem] md:ml-[40rem] lg:ml-[10rem] xl:mt-[20rem] xl:ml-[34rem] lg:mt-[20rem] md:mt-[80rem] sm:mt-[30rem] xs:mt-[20rem]  xl:grid lg:grid md:grid sm:grid xs:grid">Download CV</button>
       </div>
   );
 };
