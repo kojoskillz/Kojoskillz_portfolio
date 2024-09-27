c0203d9a8215ac7b1fdf96b03b9f252d8bf53854
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-09-27 07:35:12 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import Navbar from "../components/Navbar";
 import SparklesPreview from "../components/Homepage";
-import NumberTickerDemo from "../components/Number_ticker";
+import NumberTickerDemo  from "../components/Number_ticker";
 import BlurInDemo from "../components/Blur_In";
 import TimelineDemo  from "../components/Timeline";
 import Technical_skills  from "../components/Technical_skills";
 import VortexDemo  from "../components/Vortex";
 import { AnimatedPinDemo } from "../components/Project_card";
-import '../app/globals.css';
 import { BentoGridSecondDemo } from "../components/Bento_Grid";
 import { GlobeDemo } from "../components/Globe";
+import '../app/globals.css';
 
 
 
