8ea455018a0ff98636da322a30b77217e9918ec6
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-09-27 07:46:44 +0000

    Update Number_ticker.tsx

--- src/components/Number_ticker.tsx
+++ src/components/Number_ticker.tsx
@@ -1,30 +1,30 @@
- "use client";
- import React from "react";
- import NumberTicker from "../components/magicui/number-ticker";
+//  "use client";
+//  import React from "react";
+//  import NumberTicker from "../components/magicui/number-ticker";
 
-export default function  NumberTickerDemo() {
-  return (
-    <div className="mb-10 mt-16">
-    <div className="whitespace-pre-wrap text-8xl m-auto xl:flex lg:flex md:flex sm:grid xs:grid grid place-content-center overflow-x-hidden overflow-y-hidden gap-10 font-medium tracking-tighter bg-black   text-white dark:text-white">
-     <div className="border-2 border-white md:w-[20rem] lg:w-[20rem]  grid place-content-center p-5 rounded-3xl  border-solid ">
-         <NumberTicker value={165}  />
-         <h1 className="text-white/80 font-sans text-[16px]  text-center tracking-wide font-light" >Projects Completed</h1>
-     </div>
+// export default function  NumberTickerDemo() {
+//   return (
+//     <div className="mb-10 mt-16">
+//     <div className="whitespace-pre-wrap text-8xl m-auto xl:flex lg:flex md:flex sm:grid xs:grid grid place-content-center overflow-x-hidden overflow-y-hidden gap-10 font-medium tracking-tighter bg-black   text-white dark:text-white">
+//      <div className="border-2 border-white md:w-[20rem] lg:w-[20rem]  grid place-content-center p-5 rounded-3xl  border-solid ">
+//          <NumberTicker value={165}  />
+//          <h1 className="text-white/80 font-sans text-[16px]  text-center tracking-wide font-light" >Projects Completed</h1>
+//      </div>
 
-     <div className="border-2 border-white  md:w-[20rem] lg:w-[20rem]  grid place-content-center p-5 rounded-3xl  border-solid ">
-         <NumberTicker value={55} />
-         <h1 className="text-white/80 font-sans text-[16px]  text-center tracking-wide font-light" >Awards</h1>
-     </div>
+//      <div className="border-2 border-white  md:w-[20rem] lg:w-[20rem]  grid place-content-center p-5 rounded-3xl  border-solid ">
+//          <NumberTicker value={55} />
+//          <h1 className="text-white/80 font-sans text-[16px]  text-center tracking-wide font-light" >Awards</h1>
+//      </div>
 
-     <div className="border-2 border-white md:w-[20rem] lg:w-[20rem]  grid place-content-center   p-5 rounded-3xl  border-solid ">
-         <NumberTicker value={4253} />
-         <h1 className="text-white/80 font-sans text-[16px] text-center tracking-wide font-light" >Codes Committed and  <br /> still counting...</h1>
-     </div>
-    </div>
+//      <div className="border-2 border-white md:w-[20rem] lg:w-[20rem]  grid place-content-center   p-5 rounded-3xl  border-solid ">
+//          <NumberTicker value={4253} />
+//          <h1 className="text-white/80 font-sans text-[16px] text-center tracking-wide font-light" >Codes Committed and  <br /> still counting...</h1>
+//      </div>
+//     </div>
     
-    </div>
+//     </div>
    
-  );
-};
+//   );
+// };
 
 
