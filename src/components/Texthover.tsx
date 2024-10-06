13612bfc336b1bbcfda9513c44aaf703fbf04273
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-10-06 11:34:51 +0000

    Add files via upload

---
+++ src/components/Texthover.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { TextHoverEffect } from "../components/ui/text-hover-effect";
+
+export default function TextHoverEffectDemo() {
+  return (
+    <>
+      {/* <TextHoverEffect text="JUST IMAGINE IT" /> */}
+    </>
+  );
+}
